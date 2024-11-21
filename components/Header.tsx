d6dd3a929a8f787fb08176cbe89601acc48a6031
Rania Chettab <40768817+raniaCht@users.noreply.github.com>
2024-11-21 15:12:04 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,23 @@
-import { Globe, Headset, LogIn, UserRoundPlus } from "lucide-react";
+import { Globe, Headset, LogIn, UserRoundPlus, Menu} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 function Header() {
   return (
-    <header className="text-black flex justify-around items-center py-4 container mx-auto">
+    <header className="text-black flex justify-between lg:justify-around items-center py-4 container mx-auto">
       <h1 className="flex justify-center items-center gap-2 text-xl font-bold">
         <Image src="/logo.png" width={50} height={50} alt="logo" />
         <span>Study Platform</span>
       </h1>
-      <nav className="flex justify-center items-center gap-3 font-normal">
+      <Menu className="lg:hidden block" />
+      <nav className="hidden lg:flex justify-center items-center gap-3 font-normal">
         <Link href="">Home</Link>
         <Link href="">Blog</Link>
         <Link href="">FAQs</Link>
         <Link href="">Contact us</Link>
       </nav>
-      <div className="flex justify-center items-center">
+      <div className="hidden lg:flex justify-center items-center">
         <div className="flex justify-center items-center gap-2 pe-3 me-2 border-e border-black border-opacity-10">
           <button className="flex justify-center items-center gap-1 px-4 py-2 rounded-lg bg-black text-gray-50">
             <UserRoundPlus />
